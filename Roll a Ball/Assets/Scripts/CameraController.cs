7bbb7f52cc9ff1f32f9d91ce9022b5741419b5af
endormoon <endormoon@gmail.com>
2018-03-11 11:34:02 +0100

    Unity 3D Roll a Ball

---
+++ Roll a Ball/Assets/Scripts/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	//The reference to the ball (Player Object)
+	public GameObject player;
+
+	//Distance between the camera and the ball (Player Object)
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		//Offset = vector with the distance between the camera and the ball (Player Object)
+		offset = transform.position - player.transform.position;
+	}
+	
+	// It's like Update (is called once per frame), but it is guaranteed to run after all items have been processed in Update
+	void LateUpdate () {
+		transform.position = player.transform.position + offset;
+	}
+}
---
+++ Roll a Ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+
+	public Text countText;
+
+	public Text winText;
+
+	public Rigidbody rb;
+
+	private int count;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+		count = 0;
+		setCountText ();
+		winText.text = "";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	// Just called before physics calculations
+	void FixedUpdate () 
+	{
+		//Capturamos el movimiento horizontal desde un keyboard o joystick
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		//Capturamos el movimiento vertical desde un keyboard o joystick
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		//Determinamos la fuerza del movimiento de la bola (x,y,z)
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		//Vector3
+		rb.AddForce (movement * speed);
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.CompareTag ("Pick Up")) 
+		{
+			other.gameObject.SetActive (false);
+			count = count + 1;
+			setCountText ();
+		}
+	}
+
+	void setCountText()
+	{
+		countText.text = "Count: " + count.ToString ();
+		if (count >= 12) 
+		{
+			winText.text = "You Win!!!";
+		}
+	}
+}
